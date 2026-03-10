fd8cd7ea69777e6d92551a0556911a9a7946aa8a
MomDra <yem9087@gmail.com>
2026-03-10 22:07:45 +0900

    총알이 안나가는 버그 수정

--- ProjectAT/Assets/Scripts/Utils/PoolManager.cs
+++ ProjectAT/Assets/Scripts/Utils/PoolManager.cs
@@ -26,8 +26,16 @@ public class PoolManager
     {
         GameObject returnObject = GetObject(prefab);
 
-        returnObject.transform.position = position;
-        returnObject.transform.rotation = rotation;
+        if (returnObject.TryGetComponent(out Rigidbody rigidbody))
+        {
+            rigidbody.position = position;
+            rigidbody.rotation = rotation;
+        }
+        else
+        {
+            returnObject.transform.position = position;
+            returnObject.transform.rotation = rotation;
+        }
 
         if (returnObject.TryGetComponent(out PooledObject pooledObject))
         {
